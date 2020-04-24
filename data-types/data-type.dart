25932e034fdd3627f5088fb2666f1531bfe2e87d
Rafael Corazzi <rafael.corazzi@hotmail.com>
2020-04-24 19:37:42 -0300

    datat types

--- data-types/data-type.dart
+++ data-types/data-type.dart
@@ -2,6 +2,12 @@ void main() {
 
   int number = 54;
   bool is_active = true;
+  int hexValue= 0xEADEBAEE;
+  double exponent = 1.32e4;
+  String name = "Rafael Corazzi";
+  print(name);
+  print(exponent);
+  print(hexValue);
   print(number);
   print(is_active);
 }
