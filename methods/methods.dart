31bb24bad07eb073f8c5197cf1b847c7be870959
Rafael Corazzi <rafael.corazzi@hotmail.com>
2020-04-25 10:03:05 -0300

    optional

--- methods/methods.dart
+++ methods/methods.dart
@@ -2,6 +2,21 @@ void main(){
   int teste = findArea(3, 4);
   print(teste);
   print(area(3,6));
+  printCountries("USA", "BRAZIL");
+  printCountries("USA", "BRAZIL", "INDIA");
+  printCities("Dallas");
+  printCities("Texas", "Maranhao");
+  findVolume(1, 2, heigth: 10);
+  findVolume(1, 2);
+}
+
+
+//optional parameters
+void findVolume(int length, int breadth, {int heigth}){
+   print("Length is ${length}");
+   print("breadth is ${breadth}");
+   print("heigth is ${heigth}");
+  
 }
 
 int findArea(int length, int breadth){
@@ -9,3 +24,16 @@ int findArea(int length, int breadth){
 }
 
 int area(a, b)  =>  a * b;
+
+//optional positional parameters
+void printCountries(String name1, String name2, [String name3]){
+   print("${name1}");
+   print("${name2}");
+   print("${name3}");
+}
+
+void printCities(String name1, [String name2, String name3]){
+   print("${name1}");
+   print("${name2}");
+   print("${name3}");
+}
